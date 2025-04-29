03c7f956a8764ca256ae93610d0f0db134de2999
ZeBaptista <146209379+ZeBaptista@users.noreply.github.com>
2025-04-29 19:12:41 -0300

    refactor: unifica telas de documentos em uma única tela Documents_Unified

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -5,11 +5,6 @@ import { RootStackParamList } from '../types/navigation';
 import { Abre } from '../screens/Abre';
 import { Welcome } from '../screens/Welcome';
 import { Abre_documents } from '../screens/Abre_documents';
-import { Documents_Home } from '../screens/Documents/Documents_Home';
-import { Documents_Upload } from '../screens/Documents/Documents_Upload';
-import { Documents_Uploading } from '../screens/Documents/Documents_Uploading';
-import { Documents_Complete } from '../screens/Documents/Documents_Complete';
-import { Documents_ListWithFiles } from '../screens/Documents/Documents_ListWithFiles';
 import { Documents_Unified } from '../screens/Documents/Unified/Documents_Unified';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -26,11 +21,6 @@ export const AppNavigator = () => {
         <Stack.Screen name="Abre" component={Abre} />
         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="Abre_documents" component={Abre_documents} />
-        <Stack.Screen name="Documents_Home" component={Documents_Home} />
-        <Stack.Screen name="Documents_Upload" component={Documents_Upload} />
-        <Stack.Screen name="Documents_Uploading" component={Documents_Uploading} />
-        <Stack.Screen name="Documents_Complete" component={Documents_Complete} />
-        <Stack.Screen name="Documents_ListWithFiles" component={Documents_ListWithFiles} />
         <Stack.Screen name="Documents_Unified" component={Documents_Unified} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,9 +2,5 @@ export type RootStackParamList = {
   Abre: undefined;
   Welcome: undefined;
   Abre_documents: undefined;
-  Documents_Home: undefined;
-  Documents_Upload: undefined;
-  Documents_Uploading: { documentName?: string };
-  Documents_ListWithFiles: undefined;
   Documents_Unified: undefined;
 }; 

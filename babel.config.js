5880861417d75fa4a031af6677706d94913cc07d
ZeBaptista <jrbj001@gmail.com>
2025-05-08 15:19:44 -0300

    ajuste para sempre usar yarn

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ["nativewind/babel", { mode: "compileOnly" }],
-      "@babel/plugin-syntax-jsx"
-    ],
+    plugins: ["nativewind/babel"],
   };
 }; 
